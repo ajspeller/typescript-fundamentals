fc8196ab750f74f4767d40a73dbd043266855112
AJ <ajspeller@gmail.com>
2018-04-22 10:38:04 -0400

    Objects

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,15 @@
 var Point = /** @class */ (function () {
     function Point() {
     }
+    Point.prototype.draw = function () {
+        console.log(this.x, this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+        // ...
+    };
     return Point;
 }());
+var point = new Point();
+point.x = 88;
+point.y = 34;
+point.draw();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,11 +1,16 @@
 
 class Point {
-    x: number;
-    y: number; // properties  or fields
-    draw: () => { // method
-        // ...
-    };
-    getDistance: (another: Point) => { // another method
+    x: number; // field
+    y: number; // field
+    draw() { // method
+        console.log(this.x, this.y);
+    }
+    getDistance(another: Point) { // another method
         // ...
     }
 }
+
+let point: Point = new Point();
+point.x = 88;
+point.y = 34;
+point.draw();
