c1842b762f0b3fd3c5f3b19cd9e05bf3da7e851c
AJ <ajspeller@gmail.com>
2018-04-22 09:37:21 -0400

    Classes

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,17 +1,5 @@
-// one method to ensure that the correct type of object is passed
-// inline type annonation
-var drawPoint = function (point) {
-    // ...
-};
-drawPoint({
-    x: 2,
-    y: 88
-});
-;
-var drawPoint2 = function (point) {
-    // ...
-};
-drawPoint2({
-    x: 2,
-    y: 88
-});
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    return Point;
+}());
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,23 +1,11 @@
 
-// one method to ensure that the correct type of object is passed
-// inline type annonation
-let drawPoint = (point: { x: number, y: number }) => {
-    // ...
-};
-
-drawPoint({
-    x: 2,
-    y: 88
-});
-
-// or use an interface 
-interface Point { x: number, y: number };
-
-let drawPoint2 = (point: Point) => {
-    // ...
-};
-
-drawPoint2({
-    x: 2,
-    y: 88
-});
+class Point {
+    x: number;
+    y: number; // properties  or fields
+    draw: () => { // method
+        // ...
+    };
+    getDistance: (another: Point) => { // another method
+        // ...
+    }
+}
