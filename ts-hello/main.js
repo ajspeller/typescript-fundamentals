48643913437ac5930718af1c8d7e79cfd35f33b5
AJ <ajspeller@gmail.com>
2018-04-22 11:16:54 -0400

    Properties

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,12 +1,39 @@
 var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
     Point.prototype.draw = function () {
-        console.log(this.x, this.y);
+        console.log(this._x, this._y);
     };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0) {
+                throw new Error('Value cannot be negative');
+            }
+            else {
+                this._x = value;
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "y", {
+        get: function () {
+            return this._y;
+        },
+        set: function (value) {
+            // ... not implemented
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
-var point = new Point(23, 56);
+var point = new Point(23, -56);
+var x = point.x;
+point.x = -99;
 point.draw();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,10 +1,33 @@
 
 class Point {
-    constructor(private x?: number, private y?: number) { }
+    constructor(private _x?: number, private _y?: number) { }
     draw() { // method
-        console.log(this.x, this.y);
+        console.log(this._x, this._y);
     }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        if (value < 0) {
+            throw new Error('Value cannot be negative')
+        } else {
+            this._x = value;
+        }
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        // ... not implemented
+    }
+
 }
 
-let point = new Point(23, 56);
+let point = new Point(23, -56);
+let x = point.x;
+point.x = -99;
 point.draw();
